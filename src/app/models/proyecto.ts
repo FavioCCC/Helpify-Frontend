e95cb698c46e22905979bde4fb0359979cc117e8
Maria José Elorrieta Carrión <u202311409@upc.edu.pe>
2025-10-31 01:35:19 -0500

    Create proyecto.ts
    Model proyecto

---
+++ src/app/models/proyecto.ts
@@ -0,0 +1,13 @@
+export class Proyecto {
+  idproyecto: number;
+  nombreProyecto: string;
+  descripcion: string;
+  montoobjetivo:number;
+  montorecaudado:number;
+  fechaInicio: Date;
+  fechaFin: Date;
+  nombreorganizacion: string;
+  escuelabeneficiada: string;
+  cupoMaximo:number;
+  imagen: string;
+}
