064ae0bcc5a5a3acaff7fdef435f0f4a340f9b4f
Maria José Elorrieta Carrión <u202311409@upc.edu.pe>
2025-10-31 23:31:38 -0500

    Arreglo de proyectos y creacion de pantalla iniciar sesion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,14 @@ import { Routes } from '@angular/router';
 import {Home} from './components/home/home';
 import {ListarProyectos} from './components/proyecto/listarproyectos/listarproyectos';
 import { ComentarioComponent } from './components/comentario/foro/comentario';
+import {Iniciarsesion} from './components/usuario/iniciarsesion/iniciarsesion';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: Home },
   { path: 'proyectos', component: ListarProyectos},
   { path: 'foros', component: ComentarioComponent },
+  //Corregir porque puse eso para poder ver mi pantalla de login
+  { path: 'login', component: Iniciarsesion },
   {path: '**', redirectTo: ''}
 ];
