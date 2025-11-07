9a0bd2e857fff1f158fbe96dbf30e1379d19bebf
FavioCCC <u202310792@upc.edu.pe>
2025-11-07 15:40:17 -0500

    Incorporación de atributo usuario en universitario.ts

--- src/app/models/universitario.ts
+++ src/app/models/universitario.ts
@@ -1,5 +1,8 @@
+import {Usuario} from './usuario';
+
 export class Universitario {
   iduniversitario: number;
   idusuario: number;
-  codigoestudiante!: string;
+  codigoestudiante!: string; // opcional, si lo necesitas para alguna relación rápida
+  usuario?: Usuario;        // opcional, si lo necesitas para mostrar detalles
 }
